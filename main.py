7df5e8e9dffa4b86fb08fc546a455aaa1ec42c25
Thor Limberg <thor.limberg@qbeyond.de>
2023-01-19 13:20:22 +0100

    Changed start page

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse
+from jinja2 import Template
 
 app = FastAPI()
 
 
-@app.get("/")
+template = """
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>Test:</h1>
+    entries:
+    <ul>
+    {% for entry in the_list %}
+      <li>{{entry}}</li>
+    {% endfor %}
+    </ul>
+    <code>Var: {{var}}</code>
+    <br>
+    <kbd>Opened: {{opened}}</kbd>
+  </body>
+</html>
+"""
+
+j2_template = Template(template)
+
+opened = 0
+
+
+@app.get("/", response_class = HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    for i in range(3):
+        print('hello')
+    print('bye')
+    global opened
+    opened += 1
+    return j2_template.render(
+            {
+                'the_list': [
+                    'so',
+                    'much',
+                    'stuff',
+                    'in',
+                    'here'
+                ],
+                'var': 'so variable',
+                'opened': opened
+            }
+        )
 
 
 @app.get("/hello/{name}")
