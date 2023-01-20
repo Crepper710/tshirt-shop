fee99e9f24e0b8dfaf961a75392021f7230d8153
Thor Limberg <thor.limberg@qbeyond.de>
2023-01-20 12:53:00 +0100

    outsourced common structure

--- app/sites/index.py
+++ app/sites/index.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
-from app.utils import j2_environment
+from app.utils import j2_environment, empty_site
 
 
 router = APIRouter()
@@ -10,4 +10,8 @@ index_template = j2_environment.get_template("index.j2")
 
 @router.get("/", response_class = HTMLResponse)
 def index():
-    return index_template.render()
+    return empty_site.render({
+        "site": index_template.render({
+            "stuff": "lol"
+        })
+    })
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,7 @@
 from jinja2 import FileSystemLoader, Environment
 
 
+_j2_loader = FileSystemLoader(searchpath = "./app/")
+empty_site = Environment(loader = _j2_loader).get_template("empty_site.j2")
 _j2_loader = FileSystemLoader(searchpath = "./app/sites/")
 j2_environment = Environment(loader = _j2_loader)
