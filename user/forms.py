025ee4daf056052e1175d583588f2e7542bdcb83
Thor Limberg <thor.limberg@qbeyond.de>
2023-01-27 17:12:43 +0100

    added sign up db connection (incomplete)

--- user/forms.py
+++ user/forms.py
@@ -1,13 +1,20 @@
 from django.forms import Form, CharField
+from django.core.validators import RegexValidator
+
+
+word_validator = RegexValidator(r"^(?:[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEFA-Za-z. \-]{0,128})$")
+email_validator = RegexValidator(
+    r"^(?:[\w-]+(?:\.[\w-]+)*)@(?:(?:[\w-]+\.)*\w[\w-]{0,66})\.(?:[a-z]{2,6}(?:\.[a-z]{2})?)$")
+house_number_validator = RegexValidator(r"^[1-9][0-9]*[a-zA-Z]*$")
 
 
 class SignUpForm(Form):
-    firstname = CharField(label="First Name", max_length=128)
-    lastname = CharField(label="Last Name", max_length=128)
-    email = CharField(label="E-Mail", max_length=128)
-    password = CharField(label="Password", max_length=128)
-    password_confirm = CharField(label="Password Confirm", max_length=128)
-    street = CharField(label="Street", max_length=128)
-    house_number = CharField(label="House Number", max_length=128)
-    zip_code = CharField(label="ZIP Code", max_length=128)
-    town = CharField(label="Town", max_length=128)
+    firstname = CharField(label="First Name", max_length=128, validators = [word_validator])
+    lastname = CharField(label="Last Name", max_length=128, validators = [word_validator])
+    email = CharField(label="E-Mail", max_length=128, validators = [email_validator])
+    password = CharField(label="Password", max_length=128, min_length = 8)
+    password_confirm = CharField(label="Password Confirm", max_length=128, min_length = 8)
+    street = CharField(label="Street", max_length=128, validators = [word_validator])
+    house_number = CharField(label="House Number", max_length=128, validators = [house_number_validator])
+    zip_code = CharField(label="ZIP Code", max_length=128)  # no regex possible (to different around the world)
+    town = CharField(label="Town", max_length=128, validators = [word_validator])
--- user/views.py
+++ user/views.py
@@ -2,13 +2,49 @@ from django.http import HttpRequest
 from django.shortcuts import render, HttpResponse
 from django.template import loader
 from .forms import SignUpForm
+import db
 
 
 def signup(request: HttpRequest):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            print(request.POST)
+            firstname = form.cleaned_data.get("firstname")
+            lastname = form.cleaned_data.get("lastname")
+            password = form.cleaned_data.get("password")
+            password_c = form.cleaned_data.get("password_confirm")
+            email = form.cleaned_data.get("email")
+            street = form.cleaned_data.get("street")
+            house_number = form.cleaned_data.get("house_number")
+            zip_code = form.cleaned_data.get("zip_code")
+            town = form.cleaned_data.get("town")
+
+            if password != password_c:
+                pass  # TODO throw error
+            with db.open_connection() as con:
+                con: db.connection
+                with con.cursor() as cur:
+                    cur: db.cursor
+
+                    cur.execute("SELECT * FROM benutzer WHERE email = %(email)s", {"email": email})
+                    temp = cur.fetchall()
+
+                    if len(temp) != 0:
+                        pass  # TODO throw error
+
+                    cur.execute(
+                        """INSERT INTO benutzer (name, strasse, plz, ort, email, passwort)
+                        VALUES (%(name)s, %(street)s, %(zip_code)s, %(town)s, %(email)s, %(password)s)""",
+                        {
+                            "name": f"{firstname} {lastname}",
+                            "password": password,
+                            "email": email,
+                            "street": f"{street} {house_number}",
+                            "zip_code": zip_code,
+                            "town": town
+                        }
+                    )
+            print()
     empty_template = loader.get_template("empty_site.html")
     template = loader.get_template("user/signup.html")
     return HttpResponse(empty_template.render({"site": template.render({}, request)}, request))
